d6027d17a21e0feb98d068ad85bf0d7a67fcf69b
Adriana-st <s00273868@atu.ie>
2026-09-23 15:59:16 +0100

    worksheet 3 done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express, {Application, Request, Response} from "express" ;
 import carRoutes from './routes/cars';
 import { env } from "./config/env";
+import { connectDB } from "./config/database";
 
 const port = env.port;
 
+
 const app: Application = express();
 
-app.use('/api/v1/cars', carRoutes);
 app.use(express.json());
+app.use('/api/v1/cars', carRoutes);
 
 app.use((req, _res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
@@ -38,6 +40,13 @@ app.get('/oranges', async (_req : Request, res: Response) => {
     });
 });
 
-app.listen(port, () => {
-    console.log("Server is running on port", port);
-    });
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+};
+
+startServer();
--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,9 @@ dotenv.config();
 
 export const env = {
 
-port: Number(process.env.PORT ?? 3001),
-nodeEnv: process.env.NODE_ENV ?? "development",
-
+    port: Number(process.env.PORT ?? 3001),
+    nodeEnv: process.env.NODE_ENV ?? "development",
+    mongoURI: process.env.MONGODB_URI ?? ""
 };
+
+
